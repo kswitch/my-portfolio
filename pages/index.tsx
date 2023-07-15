eb4901d4a174bd699f3124a41b6325fa8372b97f
Kingsley Osuagwu-Chidiadi <chidiadi.kingsley.com>
2023-07-15 20:41:27 +0100

    Adjusted title in header

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,10 @@ import MyWorks from '@/components/works/MyWorks'
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <>
+  return <>
       <Head>
-        <title>Kingsley's Portfolio</title>
-        <meta 
-          name="description" 
-          content="Kingsley is a versatile Front-End engineer based in Lagos, Nigeria with specialty in React and NextJS. 
-            He also has a solid grip of the fundamentals of web development using HTML, CSS, and JavaScript. 
-            He loves to code using TypeScript" 
+        <title>Kingsley&apos;s Portfolio</title>
+        <meta name="description" content="Kingsley is a versatile Front-End engineer based in Lagos, Nigeria with specialty in React and NextJS. He also has a solid grip of the fundamentals of web development using HTML, CSS, and JavaScript. He loves to code using TypeScript" 
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* <link></link> Remeber to put a fav icon here*/}
@@ -21,5 +16,4 @@ export default function Home() {
         <MyWorks />
       </main>
     </>
-  )
 }
