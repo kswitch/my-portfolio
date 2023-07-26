c83239639841b29bc5ac94b806bef29edff9e4df
Kingsley Osuagwu-Chidiadi <chidiadi.kingsley@gmail.com>
2023-07-26 12:09:15 +0100

    Minor Style Adjustments

--- components/works/MyWorks.tsx
+++ components/works/MyWorks.tsx
@@ -1,3 +1,4 @@
+import Wrapper from '../paddingWrapper/Wrapper'
 import SectionHeader from '../sectionHeader/SectionHeader'
 import WorkItem from './WorkItem'
 import styles from './myworks.module.css'
@@ -7,44 +8,52 @@ export default function MyWorks (): JSX.Element {
         <div className={`${styles.container}`} id='works'>
             <SectionHeader value="My Works"/>
             <div className={styles.worksList}>
-                <WorkItem 
-                    imgSrc= "/images/quiz.jpg"
-                    imgAlt= "Quizzical App Image"
-                    title= "Quizzical App"
-                    excerpt= "A Trivia App that is fun to play with friends. Pick your category and play."
-                    languages={['HTML', 'CSS', 'JavaScript', 'React.JS']}
-                    weblink="https://kswitch-quizzical-trivia-app.netlify.app"
-                    githubLink="https://github.com/kswitch/quizzical-trivia-app"
-                    priority= {true}
-                />
-                <WorkItem 
-                    imgSrc= "/images/portfolio-webpage.png"
-                    imgAlt= "Portfolio Image"
-                    title= "My Portfolio"
-                    excerpt= "A Portfolio built using Next.JS and TypeScript"
-                    languages={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Next.JS']}
-                    weblink="https://kswitch.netlify.app"
-                    githubLink="https://github.com/kswitch/my-portfolio"
-                    priority= {true}
-                />
-                <WorkItem 
-                    imgSrc= "/images/weather-forecast.png"
-                    imgAlt= "Weather Forecast Image"
-                    title= "Weather Forecast Website"
-                    excerpt= "A weather forecast website built using Vite and React. You can check view the 5-day forecast for any city in the world"
-                    languages={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React.JS']}
-                    weblink="https://kswitch-weather-forecast-website.netlify.app"
-                    githubLink="https://github.com/kswitch/weather-forecast-website"
-                />
-                <WorkItem 
-                    imgSrc= "/images/speed-typing.png"
-                    imgAlt= "Speed Typing Game Image"
-                    title= "Speed Typing Game"
-                    excerpt= "A little web app to test your typing speed."
-                    languages={['HTML', 'CSS', 'JavaScript', 'React.JS']}
-                    weblink="https://kswitch-speed-typing-game.netlify.app"
-                    githubLink="https://github.com/kswitch/speed-typing-game"
-                />
+                <Wrapper styles={['red-bottom-border', 'padding-sm']}>
+                    <WorkItem 
+                        imgSrc= "/images/quiz.jpg"
+                        imgAlt= "Quizzical App Image"
+                        title= "Quizzical App"
+                        excerpt= "A Trivia App that is fun to play with friends. Pick your category and play."
+                        languages={['HTML', 'CSS', 'JavaScript', 'React.JS']}
+                        weblink="https://kswitch-quizzical-trivia-app.netlify.app"
+                        githubLink="https://github.com/kswitch/quizzical-trivia-app"
+                        priority= {true}
+                    />
+                </Wrapper>
+                <Wrapper styles={['red-bottom-border', 'padding-sm']}>
+                    <WorkItem 
+                        imgSrc= "/images/portfolio-webpage.png"
+                        imgAlt= "Portfolio Image"
+                        title= "My Portfolio"
+                        excerpt= "A Portfolio built using Next.JS and TypeScript"
+                        languages={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Next.JS']}
+                        weblink="https://kswitch.netlify.app"
+                        githubLink="https://github.com/kswitch/my-portfolio"
+                        priority= {true}
+                    />
+                </Wrapper>
+                <Wrapper styles={['red-bottom-border', 'padding-sm']}>
+                    <WorkItem 
+                        imgSrc= "/images/weather-forecast.png"
+                        imgAlt= "Weather Forecast Image"
+                        title= "Weather Forecast Website"
+                        excerpt= "A weather forecast website built using Vite and React. You can check view the 5-day forecast for any city in the world"
+                        languages={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React.JS']}
+                        weblink="https://kswitch-weather-forecast-website.netlify.app"
+                        githubLink="https://github.com/kswitch/weather-forecast-website"
+                    />
+                </Wrapper>
+                <Wrapper styles={['padding-sm']}>
+                    <WorkItem 
+                        imgSrc= "/images/speed-typing.png"
+                        imgAlt= "Speed Typing Game Image"
+                        title= "Speed Typing Game"
+                        excerpt= "A little web app to test your typing speed."
+                        languages={['HTML', 'CSS', 'JavaScript', 'React.JS']}
+                        weblink="https://kswitch-speed-typing-game.netlify.app"
+                        githubLink="https://github.com/kswitch/speed-typing-game"
+                    />
+                </Wrapper>
             </div>
         </div>
     )
